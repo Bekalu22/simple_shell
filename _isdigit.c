14d5ba5700732cc12c0f781c84dcdc6b7852fee0
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-11-14 21:59:19 +0300

    Update _isdigit.c

--- _isdigit.c
+++ _isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - prints 1 if it is a digit
  * @c: The character to say if is digit
- * Return: 0 if compilation is succesfully
+ * Return: 0 if compilations is succesfully
  */
 int _isdigit(int c)
 {
