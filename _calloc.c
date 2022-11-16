e33d352d1a68e4dddb1995511eea7b845769af16
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-11-16 15:42:01 +0300

    Update _calloc.c

--- _calloc.c
+++ _calloc.c
@@ -1,35 +1,154 @@
 #include "shell.h"
-
 /**
- * _calloc - is a function that allocates memory for an array, using malloc.
- * @nmemb: Number of elements.
- * @size: Bytes of elements.
- * Return: returns a pointer to the allocated memory.
- * The memory is set to zero
- * If nmemb or size is 0, then _calloc returns NULL
- * If malloc fails, then or _calloc returns NULL
+ * exit_bul - Exit Statue Shell
+ * @cmd: Parsed Command
+ * @input: User Input
+ * @argv:Program Name
+ * @c:Excute Count
+ * Return: Void (Exit Statue)
  */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+void  exit_bul(char **cmd, char *input, char **argv, int c)
 {
+	int statue, i = 0;
 
-	int i, range;
-	char *ch;
-	void *p = NULL;
-
-	range = nmemb * size;
-	if (range == 0)
-		return (NULL);
-	p = malloc(range);
-	if (p != NULL)
+	if (cmd[1] == NULL)
 	{
-		ch = (char *)p;
-		for (i = 0; i < range; i++)
-			ch[i] = 0;
-		return (p);
+		free(input);
+		free(cmd);
+		exit(EXIT_SUCCESS);
 	}
-	else
+	while (cmd[1][i])
 	{
-		return (p);
+		if (_isalpha(cmd[1][i++]) != 0)
+		{
+			_prerror(argv, c, cmd);
+			break;
+		}
+		else
+		{
+			statue = _atoi(cmd[1]);
+			free(input);
+			free(cmd);
+			exit(statue);
+		}
 	}
 }
+
+
+/**
+ * change_dir - Change Dirctorie
+ * @cmd: Parsed Command
+ * @er: Statue Last Command Excuted
+ * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
+ */
+int change_dir(char **cmd, __attribute__((unused))int er)
+{
+	int value = -1;
+	char cwd[PATH_MAX];
+
+	if (cmd[1] == NULL)
+		value = chdir(getenv("HOME"));
+	else if (_strcmp(cmd[1], "-") == 0)
+	{
+		value = chdir(getenv("OLDPWD"));
+	}
+	else
+		value = chdir(cmd[1]);
+
+	if (value == -1)
+	{
+		perror("hsh");
+		return (-1);
+	}
+	else if (value != -1)
+	{
+		getcwd(cwd, sizeof(cwd));
+		setenv("OLDPWD", getenv("PWD"), 1);
+		setenv("PWD", cwd, 1);
+	}
+	return (0);
+}
+/**
+ * dis_env - Display Enviroment Variable
+ * @cmd:Parsed Command
+ * @er:Statue of Last command Excuted
+ * Return:Always 0
+ */
+int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
+{
+size_t i;
+	int len;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		len = _strlen(environ[i]);
+		write(1, environ[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	return (0);
+}
+/**
+ * display_help - Displaying Help For Builtin
+ * @cmd:Parsed Command
+ * @er: Statue Of Last Command Excuted
+ * Return: 0 Succes -1 Fail
+ */
+int display_help(char **cmd, __attribute__((unused))int er)
+{
+	int fd, fw, rd = 1;
+	char c;
+
+	fd = open(cmd[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error");
+		return (0);
+	}
+	while (rd > 0)
+	{
+		rd = read(fd, &c, 1);
+		fw = write(STDOUT_FILENO, &c, rd);
+		if (fw < 0)
+		{
+			return (-1);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
+/**
+ * echo_bul - Excute Echo Cases
+ * @st:Statue Of Last Command Excuted
+ * @cmd: Parsed Command
+ * Return: Always 0 Or Excute Normal Echo
+ */
+int echo_bul(char **cmd, int st)
+{
+	char *path;
+	unsigned int  pid = getppid();
+
+	if (_strncmp(cmd[1], "$?", 2) == 0)
+	{
+		print_number_in(st);
+		PRINTER("\n");
+	}
+	else if (_strncmp(cmd[1], "$$", 2) == 0)
+	{
+		print_number(pid);
+		PRINTER("\n");
+
+	}
+	else if (_strncmp(cmd[1], "$PATH", 5) == 0)
+	{
+		path = _getenv("PATH");
+		PRINTER(path);
+		PRINTER("\n");
+		free(path);
+
+	}
+	else
+		return (print_echo(cmd));
+
+	return (1);
+}
+
