1e2f39539324328779e22e473782d148b89ab375
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-11-16 16:13:25 +0300

    Update path.c

--- path.c
+++ path.c
@@ -1,117 +1,110 @@
 #include "shell.h"
 /**
- * gpath - Gets the PATH environments.
- * Return: Pointer value of PATH.
+ * _strcpy - Copie Source To Destination Char
+ * @dest:Destination
+ * @src:Source
+ * Return: Copie Of Char *
  */
-char *gpath()
+char *_strcpy(char *dest, char *src)
 {
-	unsigned int i = 0, j = 5, k = 0;
-	char *s, **e = environ;
+int i;
 
-	while (e[i])
+i = 0;
+	while (src[i])
 	{
-		if (e[i][0] == 80 && e[i][3] == 72 && e[i][4] == 61)
-		{
-			s = _calloc(PATH_MAX, sizeof(char));
-			if (s == NULL)
-			{
-				write(1, "Error: failed malloc gpath\n", 27);
-				return (NULL);
-			}
-			while (e[i][j])
-			{
-				s[k] = e[i][j];
-				k++;
-				j++;
-			}
-			s[k] = '\0';
-			return (s);
-		}
+		dest[i] = src[i];
 		i++;
+	}
+dest[i] = '\0';
+return (dest);
+}
+/**
+ * _strcat - Concat Two String
+ * @dest:First String
+ * @src:Second String
+ * Return:First String + Second String Char *
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
+/**
+ * _strchr - Locate Charactere In String
+ * @s:String Search In
+ * @c:Char To Search For
+ * Return: Pointer To Char*
+ */
+char *_strchr(char *s, char c)
+{
+
+	do		{
+
+		if (*s == c)
+			{
+			break;
+			}
+		}	while (*s++);
+
+return (s);
+}
+/**
+ * _strncmp - Compare Amount (n) Of Characters Of Two Strings.
+ * @s1: A String.
+ * @s2: A String.
+ * @n: Amount Of Characters To Compare.
+ *
+ * Return: 1 If The Strings Don't Match Otherwise 0
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	if (s1 == NULL)
+		return (-1);
+	for (i = 0; i < n && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
 /**
- * splitPath - Splits PATH each ':'
- * @path: Pointer value PATH
- * Return: Pointer each directories
+ * _strdup - Duplicate A String
+ * @str:String
+ * Return: Duplicate String Failed Null
  */
-char **splitPath(char *path)
+char *_strdup(char *str)
 {
-	char **d;
-	char *carp;
-	int i = 0;
+	size_t len, i;
+	char *str2;
 
-	d = _calloc(PATH_MAX, sizeof(char));
-	if (d == NULL)
+	len = _strlen(str);
+	str2 = malloc(sizeof(char) * (len + 1));
+	if (!str2)
 	{
-		write(1, "Error: failed malloc splitPath\n", 31);
 		return (NULL);
 	}
-	carp = strtok(path, ":");
-	while (carp)
-	{
-		d[i] = carp;
-		carp = strtok(NULL, ":");
-		i++;
-	}
-	d[i] = NULL;
-	return (d);
-}
-/**
- * checkPath - check with stat in the folders PATH with command
- * @dpath: pointer array of dir in PATH
- * @argv0: pointer to cmd name
- * Return: pointer if found, NULL not found
- */
-char *checkPath(char **dpath, char *argv0)
-{
-	struct stat mao;
-	char *completline;
-	int i = 0;
 
-	while (dpath[i])
+	for (i = 0; i <= len; i++)
 	{
-		completline = unePath(dpath[i], argv0);
-		if (stat(completline, &mao) == 0)
-			return (completline);
-		i++;
+		str2[i] = str[i];
 	}
-	return (NULL);
-}
-/**
- * unePath - Concatenate command argv[0] to each directory path
- * @dpath: pointer a one dir PATH
- * @argv0: command
- * Return: pointer string dir + cmd or NULL error
- */
-char *unePath(char *dpath, char *argv0)
-{
-	int dpathlen, argv0len, len, i = 0, j = 0;
-	char *path_cmd;
 
-	dpathlen = _strlen(dpath);
-	argv0len = _strlen(argv0);
-	len = dpathlen + argv0len;
-	path_cmd = _calloc(len + 2, sizeof(char));
-	if (path_cmd == NULL)
-	{
-		write(1, "Error: malloc catpath\n", 22);
-		return (NULL);
-	}
-	while (dpath[i])
-	{
-		path_cmd[i] = dpath[i];
-		i++;
-	}
-	path_cmd[i] = '/';
-	i++;
-	while (argv0[j])
-	{
-		path_cmd[i] = argv0[j];
-		j++;
-		i++;
-	}
-	path_cmd[i] = '\0';
-	return (path_cmd);
+	return (str2);
 }
