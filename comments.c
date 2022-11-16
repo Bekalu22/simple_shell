239356f61826e408e43722ead0a80338b5944ee0
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-11-16 15:47:41 +0300

    Update comments.c

--- comments.c
+++ comments.c
@@ -1,36 +1,95 @@
 #include "shell.h"
 /**
- * comments - remove comments from the command lines.
- * @line: command line.
- * Return: the command line without comment.
-**/
-char *comments(char *line)
-{
-	int i, j = 0;
-	char *compare = " \t\r\a";
-	char aux;
+ * read_file - Read Command From File
+ * @filename:Filename
+ * @argv:Program Name
+ * Return: -1 or  0
+ */
 
-Here:
-	while (compare[j] != '\0')
+void read_file(char *filename, char **argv)
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	int counter = 0;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
 	{
-		if (line[0] == compare[j])
-		{
-			for (i = 0; line[i] != '\0'; i++)
-			{
-				aux = line[i + 1];
-				line[i] = aux;
-			}
-			goto Here;
-		}
-		j++;
+		exit(EXIT_FAILURE);
 	}
-	for (i = 0; line[i] != '\0'; i++)
+	while ((getline(&line, &len, fp)) != -1)
 	{
-		if (line[i] == '#')
+		counter++;
+		treat_file(line, counter, fp, argv);
+
+	}
+	if (line)
+		free(line);
+	fclose(fp);
+	exit(0);
+}
+/**
+ * treat_file - PARSE Check Command Fork Wait Excute in Line of File
+ * @line: Line From A File
+ * @counter:Error Counter
+ * @fp:File Descriptor
+ * @argv:Program Name
+ * Return : Excute A line void
+ */
+void treat_file(char *line, int counter, FILE *fp, char **argv)
+{
+	char **cmd;
+	int st = 0;
+
+	cmd = parse_cmd(line);
+
+		if (_strncmp(cmd[0], "exit", 4) == 0)
 		{
-			line[i] = '\0';
-			break;
+			exit_bul_for_file(cmd, line, fp);
+		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd, st);
+			free(cmd);
+		}
+		else
+		{
+			st = check_cmd(cmd, line, counter, argv);
+			free(cmd);
+		}
+}
+/**
+ * exit_bul_for_file - Exit Shell Case Of File
+ * @line: Line From A File
+ * @cmd: Parsed Command
+ * @fd:File Descriptor
+ * Return : Case Of Exit in A File Line
+ */
+void exit_bul_for_file(char **cmd, char *line, FILE *fd)
+{
+	int statue, i = 0;
+
+	if (cmd[1] == NULL)
+	{
+		free(line);
+		free(cmd);
+		fclose(fd);
+		exit(errno);
+	}
+	while (cmd[1][i])
+	{
+		if (_isalpha(cmd[1][i++]) < 0)
+		{
+			perror("illegal number");
 		}
 	}
-	return (line);
+	statue = _atoi(cmd[1]);
+	free(line);
+	free(cmd);
+	fclose(fd);
+	exit(statue);
+
+
+
 }
