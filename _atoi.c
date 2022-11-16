5faf449a0f2d278ea83707773e119144e03dccea
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-11-16 15:40:42 +0300

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,21 +1,64 @@
 #include "shell.h"
 /**
- * _atoi - Function that convert a strings to an integer
- * @s: String to be converted an integer
- * Return: Integer value
+ * check_delim - Checks If A Character Match Any Char *
+ * @c: Character To Check
+ * @str: String To Check
+ * Return: 1 Succes, 0 Failed
  */
-int _atoi(char *s)
+unsigned int check_delim(char c, const char *str)
 {
-	int x = 0, i = 0, minus = -1;
+	unsigned int i;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (s[i] == '-')
-			minus *= -1;
-		i++;
+		if (c == str[i])
+			return (1);
 	}
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-		x = (x * 10) - (s[i++] - '0');
+	return (0);
+}
 
-	return (x * minus);
+/**
+ * _strtok - Token A String Into Token (strtrok)
+ * @str: String
+ * @delim: Delimiter
+ * Return: Pointer To The Next Token Or NULL
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *ts;
+	static char *nt;
+	unsigned int i;
+
+	if (str != NULL)
+		nt = str;
+	ts = nt;
+	if (ts == NULL)
+		return (NULL);
+	for (i = 0; ts[i] != '\0'; i++)
+	{
+		if (check_delim(ts[i], delim) == 0)
+			break;
+	}
+	if (nt[i] == '\0' || nt[i] == '#')
+	{
+		nt = NULL;
+		return (NULL);
+	}
+	ts = nt + i;
+	nt = ts;
+	for (i = 0; nt[i] != '\0'; i++)
+	{
+		if (check_delim(nt[i], delim) == 1)
+			break;
+	}
+	if (nt[i] == '\0')
+		nt = NULL;
+	else
+	{
+		nt[i] = '\0';
+		nt = nt + i + 1;
+		if (*nt == '\0')
+			nt = NULL;
+	}
+	return (ts);
 }
