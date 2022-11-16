e4f126335ffa8028dfe291ab048a651633b8ed9e
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-11-16 22:58:51 +0300

    Update error_handler_two.c

--- error_handler_two.c
+++ error_handler_two.c
@@ -2,9 +2,9 @@
 
 /**
  * _erratoi - converts a string to an integer
- * @s: the string to be converted
+ * @s: the strings to be converted
  * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
+ *    -1 on error
  */
 int _erratoi(char *s)
 {
